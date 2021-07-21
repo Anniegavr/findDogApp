abc6a521a7731a2f63b1e27f515d739e8b14b918
Anniegavr <56108881+Anniegavr@users.noreply.github.com>
2021-07-21 21:18:27 +0300

    2ndUpdate

---
+++ src/main/java/com/learn/devme/postgres/Dog.java
@@ -0,0 +1,4 @@
+package com.learn.devme.postgres;
+
+public class Dog {
+}
---
+++ src/main/java/com/learn/devme/postgres/Owner.java
@@ -0,0 +1,73 @@
+package com.learn.devme.postgres;
+import javax.persistence.*;
+
+import static java.lang.StrictMath.random;
+
+@Entity
+@Table
+public class Owner {
+    @Id
+    @SequenceGenerator(
+            name = "name",
+            sequenceName = "owner_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "owner_sequence"
+    )
+    private Long id;
+    private String name;
+    private String password;
+
+    public Owner() {
+    }
+
+    public Owner(Long id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+
+    public Owner(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+//    public Owner(String name) {
+//        this.name = name;
+//     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = "3301"+password.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
